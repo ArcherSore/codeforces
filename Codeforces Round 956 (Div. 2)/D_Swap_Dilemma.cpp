b2c67440aab2b47b98b07a0cf8e491d48c6db1fc
ArcherSore <17321322251@163.com>
2024-07-26 14:18:59 +0800

    `#置换环`求逆序对奇偶性

--- Codeforces Round 956 (Div. 2)/D_Swap_Dilemma.cpp
+++ Codeforces Round 956 (Div. 2)/D_Swap_Dilemma.cpp
@@ -16,62 +16,47 @@ using namespace std;
 using LL = long long;
 using PII = pair<int, int>;
 
-const int MAXN = 1e5 + 10;
-
-int n;
-vector<LL> tr(MAXN);
-
-int lowbit(int x) {
-    return x & -x;
-}
-
-void add(int x, int c) {
-    for (int i = x; i <= n; i += lowbit(i)) {
-        tr[i] += c;
-    }
-}
-
-LL ask(int x) {
-    LL sum = 0;
-    for (int i = x; i; i -= lowbit(i)) {
-        sum += tr[i];
-    }
-    return sum;
-}
-
 int parity(vector<int> &a) {
-    tr.assign(MAXN, 0);
-    LL tot = 0;
-    for (int i = 1; i <= n; i++) {
-        tot += ask(n) - ask(a[i]);
-        add(a[i], 1);
+    int n = a.size();
+    vector<int> vis(n);
+    int p = n % 2;
+    for (int i = 0; i < n; i++) {
+        if (vis[i]) {
+            continue;
+        }
+
+        for (int j = i; !vis[j]; j = a[j]) {
+            vis[j] = 1;
+        }
+        p ^= 1;
     }
-    return tot % 2;
+    return p;
 }
 
 void solve() {
+    int n;
     cin >> n;
-    vector<int> a(n + 1), b(n + 1);
-    for (int i = 1; i <= n; i++) {
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> b[i];
     }
 
     vector<int> t1 = a, t2 = b;
-    sort(t1.begin() + 1, t1.end());
-    sort(t2.begin() + 1, t2.end());
+    sort(t1.begin() , t1.end());
+    sort(t2.begin(), t2.end());
     if (t1 != t2) {
         cout << "NO\n";
         return;
     }
 
-    for (int i = 1; i <= n; i++) {
-        a[i] = lower_bound(t1.begin() + 1, t1.end(), a[i]) - t1.begin();
+    for (int i = 0; i < n; i++) {
+        a[i] = lower_bound(t1.begin() , t1.end(), a[i]) - t1.begin();
     }
-    for (int i = 1; i <= n; i++) {
-        b[i] = lower_bound(t2.begin() + 1, t2.end(), b[i]) - t2.begin();
+    for (int i = 0; i < n; i++) {
+        b[i] = lower_bound(t2.begin(), t2.end(), b[i]) - t2.begin();
     }
 
     // cout << parity(a) << " " << parity(b) << '\n';
