cb8883a526e197a777ccb37cdc4a7738b45d0e99
ArcherSore <17321322251@163.com>
2025-01-16 15:47:38 +0800

    practice of segment tree

---
+++ Codeforces Round 980 (Div. 2)/D_Skipping.cpp
@@ -0,0 +1,181 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using LL = long long;
+using PII = pair<int, int>;
+
+template<class Info>
+struct SegmentTree {
+    int n;
+    vector<Info> info;
+    SegmentTree() : n(0) {}
+    SegmentTree(int n_, Info v_ = Info()) {
+        init(n_, v_);
+    }
+    template<class T>
+    SegmentTree(vector<T> init_) {
+        init(init_);
+    }
+    void init(int n_, Info v_ = Info()) {
+        init(vector(n_, v_));
+    }
+    template<class T>
+    void init(vector<T> init_) {
+        n = init_.size();
+        info.assign(4 << __lg(n), Info());
+        function<void(int, int, int)> build = [&](int p, int l, int r) {
+            if (r - l == 1) {
+                info[p] = init_[l];
+                return;
+            }
+            int m = (l + r) / 2;
+            build(2 * p, l, m);
+            build(2 * p + 1, m, r);
+            pull(p);
+        };
+        build(1, 0, n);
+    }
+    void pull(int p) {
+        info[p] = info[2 * p] + info[2 * p + 1];
+    }
+    void modify(int p, int l, int r, int x, const Info &v) {
+        if (r - l == 1) {
+            info[p] = v;
+            return;
+        }
+        int m = (l + r) / 2;
+        if (x < m) {
+            modify(2 * p, l, m, x, v);
+        } else {
+            modify(2 * p + 1, m, r, x, v);
+        }
+        pull(p);
+    }
+    void modify(int p, const Info &v) {
+        modify(1, 0, n, p, v);
+    }
+    Info rangeQuery(int p, int l, int r, int x, int y) {
+        if (l >= y || r <= x) {
+            return Info();
+        }
+        if (l >= x && r <= y) {
+            return info[p];
+        }
+        int m = (l + r) / 2;
+        return rangeQuery(2 * p, l, m, x, y) + rangeQuery(2 * p + 1, m, r, x, y);
+    }
+    Info rangeQuery(int l, int r) {
+        return rangeQuery(1, 0, n, l, r);
+    }
+    template<class F>
+    int findFirst(int p, int l, int r, int x, int y, F &&pred) {
+        if (l >= y || r <= x) {
+            return -1;
+        }
+        if (l >= x && r <= y && !pred(info[p])) {
+            return -1;
+        }
+        if (r - l == 1) {
+            return l;
+        }
+        int m = (l + r) / 2;
+        int res = findFirst(2 * p, l, m, x, y, pred);
+        if (res == -1) {
+            res = findFirst(2 * p + 1, m, r, x, y, pred);
+        }
+        return res;
+    }
+    template<class F>
+    int findFirst(int l, int r, F &&pred) {
+        return findFirst(1, 0, n, l, r, pred);
+    }
+    template<class F>
+    int findLast(int p, int l, int r, int x, int y, F &&pred) {
+        if (l >= y || r <= x) {
+            return -1;
+        }
+        if (l >= x && r <= y && !pred(info[p])) {
+            return -1;
+        }
+        if (r - l == 1) {
+            return l;
+        }
+        int m = (l + r) / 2;
+        int res = findLast(2 * p + 1, m, r, x, y, pred);
+        if (res == -1) {
+            res = findLast(2 * p, l, m, x, y, pred);
+        }
+        return res;
+    }
+    template<class F>
+    int findLast(int l, int r, F &&pred) {
+        return findLast(1, 0, n, l, r, pred);
+    }
+};
+
+const LL INF = 1e18;
+struct Info {
+    LL mn;
+    Info() : mn(INF) {}
+    Info(LL x) : mn(x) {}
+};
+
+Info operator+(const Info &a, const Info &b) {
+    Info c;
+    c.mn = min(a.mn, b.mn);
+    return c;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+        b[i]--;
+    }
+
+    SegmentTree<Info> seg(n + 1);
+    if (b[0] != 0) {
+        LL cur = seg.rangeQuery(b[0], b[0] + 1).mn;
+        LL new_val = min(cur, 1LL * a[0]);
+        seg.modify(b[0], Info(new_val));
+    }
+    for (int i = 1; i < n; i++) {
+        if (b[i] > i) {
+            LL x = seg.rangeQuery(i, b[i]).mn;
+            if (x >= INF) {
+                continue;
+            }
+
+            LL cur = seg.rangeQuery(b[i], b[i] + 1).mn;
+            LL new_val = min(cur, x + a[i]);
+            seg.modify(b[i], Info(new_val));
+        }
+    }
+
+    LL sum = 0, res = a[0];
+    for (int i = 0; i < n; i++) {
+        sum += a[i];
+        res = max(res, sum - seg.rangeQuery(i, i + 1).mn);
+    }
+    cout << res << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T = 1;
+    cin >> T;
+
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
